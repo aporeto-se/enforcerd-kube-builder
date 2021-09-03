63f8e5f03f7290a57107d322febc2ece7ca9d057
jodydadescott <jody@thescottsweb.com>
2021-09-03 15:33:50 -0500

    init

--- example/example.go
+++ example/example.go
@@ -1,16 +1,18 @@
-package example
+package main
 
 import (
-	"github.com/aporeto-se/pkg/core"
+	"fmt"
+
+	core "github.com/aporeto-se/enforcerd-kube-builder/pkg"
 	"gopkg.in/yaml.v2"
 )
 
 func main() {
 
-	daemonSetBuilder := core.NewEks(namespaceController.GetNamespacePath(), t.config.PrismaAPI)
+	daemonSetBuilder := core.NewEks("/tenant/cloud/cluster", "https://the_api")
 	daemonSet := daemonSetBuilder.Build()
 
-	b, _ := yaml.Marshal(daemonset.DaemonSet)
-	return b
+	b, _ := yaml.Marshal(daemonSet.DaemonSet)
+	fmt.Println(string(b))
 
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aporeto-se/enforcerd-kube-builder
 go 1.16
 
 require (
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-	k8s.io/api v0.22.1 // indirect
-	k8s.io/client-go v0.22.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.22.1
+	k8s.io/client-go v0.22.1
 )
